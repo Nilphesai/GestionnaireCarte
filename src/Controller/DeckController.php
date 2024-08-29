470e1e857d21320ab511f900d9a1971d8bf2c16a
HO Gaël <dt.gael.ho@gmail.com>
2024-08-29 10:51:57 +0200

    création Controller et form du Deck

---
+++ src/Controller/DeckController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Card;
+use App\Form\SearchCardType;
+use App\HttpClient\ApiHttpClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DeckController extends AbstractController
+{
+    #[Route('/deck', name: 'app_deck')]
+    public function index(Card $card = null, ApiHttpClient $apiHttpClient): Response
+    {
+        $card = new Card();
+        $form = $this->createForm(SearchCardType::class,$card);
+
+        $cards = $apiHttpClient->getCards();
+
+        return $this->render('deck/index.html.twig', [
+            'formSearchCard' => $form,
+            'cards' => $cards,
+        ]);
+    }
+}
---
+++ src/Form/DeckType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\card;
+use App\Entity\Deck;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DeckType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('closed')
+            ->add('picture')
+            ->add('card', EntityType::class, [
+                'class' => card::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Deck::class,
+        ]);
+    }
+}
